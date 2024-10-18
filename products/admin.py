a144f2a866603534e447062d57293b8fde03f8ec
Alexandra Lee <alexandraelee9@gmail.com>
2024-10-18 12:34:37 +0000

    add admin fields for management in admin

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,39 @@ from django.contrib import admin
 from .models import Product, Category, ProductSize
 
 # Register your models here.
+
+# product admin
+# displays product fields
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image'
+    )
+    # order list of products by sku
+    ordering = ('sku',)
+
+# category admin
+# display both names so easy to manage
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+# productsize admin
+# display productsize fields in admin
+class ProductSizeAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'size',
+        'price',
+    )
+
+
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(ProductSize)
