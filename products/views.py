e8992e1370dbcbdb86ccf0d1b48045d7e9416be2
Alexandra Lee <alexandraelee9@gmail.com>
2024-10-23 09:48:17 +0000

    add back to top button, fix font awesom link for icons

--- products/views.py
+++ products/views.py
@@ -8,7 +8,6 @@ from django.db.models.functions import Lower
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-
     products = Product.objects.all()
     query = None
     categories = None
@@ -16,50 +15,29 @@ def all_products(request):
     direction = None
 
     # Fetch all categories for display purposes
-    all_categories = Category.objects.all()
+    all_categories = Category.objects.all()  # This gets all categories
 
+    # Initialize current_categories as a queryset or list
+    current_categories = all_categories  # Set to all categories or filter based on user selection
+
+    # Process GET requests for filtering
     if request.GET:
-        if 'sort' in request.GET:
-            sortkey = request.GET['sort']
-            sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))
-            if sortkey == 'category':
-                sortkey = 'category__name'
-
-            if 'direction' in request.GET:
-                direction = request.GET['direction']
-                if direction == 'desc':
-                    sortkey = f'-{sortkey}'
-            products = products.order_by(sortkey)
-
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
+            current_categories = Category.objects.filter(name__in=categories)  # Update current categories
 
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('products'))
-            
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
-            products = products.filter(queries)
-
-    current_sorting = f'{sort}_{direction}'
+        # ... additional filtering logic ...
 
     context = {
         'products': products,
         'search_term': query,
-        'current_categories': categories,  # Categories selected by the user
-        'all_categories': all_categories,   # All categories for badges
-        'current_sorting': current_sorting, 
+        'current_categories': current_categories,  # Ensure this is a list of categories
+        'all_categories': all_categories,  # All categories for badges
+        'current_sorting': f'{sort}_{direction}',
     }
 
     return render(request, 'products/products.html', context)
-
 
 
 def product_detail(request, product_id):
