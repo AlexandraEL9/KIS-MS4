042b9ed52af569d0426b5aa086071822523efd8f
Alexandra Lee <alexandraelee9@gmail.com>
2024-10-29 18:32:54 +0000

    complete size logic

--- bag/views.py
+++ bag/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render, redirect
 
 def view_bag(request):
     """ A view that renders the bag contents page """
-
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
@@ -12,12 +11,23 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = request.POST.get('product_size', None)
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    else:
-        bag[item_id] = quantity
+    # Ensure bag[item_id] is always a dictionary structure
+    if item_id not in bag:
+        bag[item_id] = {'items_by_size': {}, 'quantity': 0}
 
+    if size:
+        # Handle items with sizes
+        if size in bag[item_id]['items_by_size']:
+            bag[item_id]['items_by_size'][size] += quantity
+        else:
+            bag[item_id]['items_by_size'][size] = quantity
+    else:
+        # Handle items without sizes
+        bag[item_id]['quantity'] += quantity
+
+    # Update the session
     request.session['bag'] = bag
     return redirect(redirect_url)
