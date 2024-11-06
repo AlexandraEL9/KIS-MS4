a027faad337d6779f50cf15db4280ea05012329c
Alexandra Lee <alexandraelee9@gmail.com>
2024-11-06 13:47:38 +0000

    add stripe keys

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/check_out.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QI8onAS7iZraKTaGiCMEPaPNXV8czKkvfNYBb1VYvJnACK1LnTUNMImFuIcJ1GSWhvQ5r5OtCXKusquVaXMerKi00kRLscZzs',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
