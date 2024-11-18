ba160e71e88edd3de6cda8de6a6ef6a736a3683c
Alexandra Lee <alexandraelee9@gmail.com>
2024-11-18 15:14:44 +0000

    add timezone utility from django for date created field

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Subscriber(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True, max_length=100)
     date_subscribed = models.DateTimeField('Date created', default=timezone.now)
-    
+
     def __str__(self):
         return self.email
