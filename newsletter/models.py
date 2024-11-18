11c56f6a049eefb12412fbbac8cad81b95447788
Alexandra Lee <alexandraelee9@gmail.com>
2024-11-18 14:24:37 +0000

    create models for newsletter app

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Subscriber(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True, max_length=100)
+    date_subscribed = models.DateTimeField('Date created', default=timezone.now)
+    
+    def __str__(self):
+        return self.email
