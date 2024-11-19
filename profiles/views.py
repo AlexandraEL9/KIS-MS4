e9cc91df72185d52c02fc92cb898df00b4d3d9fa
Alexandra Lee <alexandraelee9@gmail.com>
2024-11-19 19:23:09 +0000

    ammend profiles views

--- profiles/views.py
+++ profiles/views.py
@@ -1,49 +1,26 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.conf import settings
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from .models import UserProfile
-from .forms import UserProfileForm
 
-from checkout.models import Order
+from profiles.models import UserProfile
+from products.models import Product
+from wishlist.models import Wishlist
 
-@login_required
-def profile(request):
-    """ Display the user's profile. """
-    profile = get_object_or_404(UserProfile, user=request.user)
+# view wishlist
+def wishlist(request):
+    """ view to show the user's wishlist """
+    if not request.user.is_authenticated:
+        messages.error(request,
+                       'Sorry, you need to be logged in to add your Wishlist.')
+        return redirect(reverse('account_login'))
 
-    if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=profile)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Profile updated successfully')
-        else:
-            messages.error(request, 'Update failed. Please ensure the form is valid.')
-    else:
-        form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
+    user = get_object_or_404(UserProfile, user=request.user)
+    wishlist = Wishlist.objects.filter(user_profile=user)
 
-    template = 'profiles/profile.html'
+    template = 'wishlist/wishlist.html'
+
     context = {
-        'form': form,
-        'orders': orders,
-        'on_profile_page': True
-    }
-
-    return render(request, template, context)
-
-
-def order_history(request, order_number):
-    order = get_object_or_404(Order, order_number=order_number)
-
-    messages.info(request, (
-        f'This is a past confirmation for order number {order_number}. '
-        'A confirmation email was sent on the order date.'
-    ))
-
-    template = 'checkout/checkout_success.html'
-    context = {
-        'order': order,
-        'from_profile': True,
+        'wishlist': wishlist,
     }
 
     return render(request, template, context)
