78634c6b421062686f8aa6ee5c8ca5f78408a429
Alexandra Lee <alexandraelee9@gmail.com>
2024-11-19 19:17:55 +0000

    update models

--- favourites/models.py
+++ favourites/models.py
@@ -3,10 +3,12 @@ from django.db import models
 from products.models import Product
 
 # Create your models here.
-class Favourite(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    added_on = models.DateTimeField(auto_now_add=True)
+class Wishlist(models.Model):
+    """ Wishlist model """
+    user_profile = models.ForeignKey(UserProfile, null=True, blank=True,
+                                     on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=True, blank=True,
+                                on_delete=models.CASCADE, default=1)
 
     class Meta:
         unique_together = ('user', 'product')
