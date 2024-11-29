0422b047b27d9484f4e20a6117ddb62805e5a9fd
Alexandra Lee <alexandraelee9@gmail.com>
2024-11-29 13:47:28 +0000

    add models to reviews app and migrate

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+class Review(models.Model):
+    """Model to represent product reviews"""
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="reviews")
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    rating = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.user.username}"
