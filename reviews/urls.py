8936e73513869a665e44571969fba4bf28f9a26e
Alexandra Lee <alexandraelee9@gmail.com>
2024-11-29 17:23:02 +0000

    add ability to update reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('<int:product_id>/', views.product_reviews, name='product_reviews'),
     path('<int:product_id>/add/', views.add_review, name='add_review'),
+    path('<int:review_id>/edit/', views.edit_review, name='edit_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -12,23 +12,40 @@ def product_reviews(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     reviews = product.reviews.all()
     return render(request, 'reviews/product_reviews.html', {'product': product, 'reviews': reviews})
-    
+
 # add product reviews
 def add_review(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
-    if request.method == 'POST':
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            review = form.save(commit=False)
-            review.product = product
-            review.user = request.user
-            review.save()
-            messages.success(request, 'Your review has been added!')
-            return redirect('product_reviews', product_id=product.id)
-    else:
-        form = ReviewForm()
+    form = ReviewForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        review = form.save(commit=False)
+        review.product = product
+        review.user = request.user
+        review.save()
+        messages.success(request, "Your review has been submitted!")
+        return redirect('product_reviews', product_id=product.id)
+
     return render(request, 'reviews/add_review.html', {'form': form, 'product': product})
 
-# update product reviews
+# update/ edit product reviews
+@login_required
+def edit_review(request, review_id):
+    """Allow a user to edit their review"""
+    review = get_object_or_404(Review, pk=review_id, user=request.user)
+    form = ReviewForm(request.POST or None, instance=review)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your review has been updated!")
+            return redirect('product_reviews', product_id=review.product.id)  # Ensure redirect happens
+        else:
+            messages.error(request, "Failed to update the review. Please check the form.")
+    
+    return render(request, 'reviews/edit_review.html', {'form': form, 'review': review})
+
+
+
 
 # delete product reviews
