cbf4209aaae29d1e9b079890bf330399d4d7996f
Alexandra Lee <alexandraelee9@gmail.com>
2024-11-29 14:08:30 +0000

    add add and view review views

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Review
+from products.models import Product
+from .forms import ReviewForm
 
 # Create your views here.
+
+# view product reviews
+def product_reviews(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    reviews = product.reviews.all()
+    return render(request, 'reviews/product_reviews.html', {'product': product, 'reviews': reviews})
+    
+# add product reviews
+def add_review(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.product = product
+            review.user = request.user
+            review.save()
+            messages.success(request, 'Your review has been added!')
+            return redirect('product_reviews', product_id=product.id)
+    else:
+        form = ReviewForm()
+    return render(request, 'reviews/add_review.html', {'form': form, 'product': product})
+
+# update product reviews
+
+# delete product reviews
