720527a90be1fcc0873272b53361b01d817fea98
Alexandra Lee <alexandraelee9@gmail.com>
2024-12-03 12:51:50 +0000

    add footer and amend subscribe page

--- subscribe/views.py
+++ subscribe/views.py
@@ -4,34 +4,17 @@ from .forms import SubscribeForm
 
 
 def subscribe(request):
-    # Check if the form has been submitted
     if request.method == 'POST':
-        # Create a SubscribeForm instance with the submitted data
-        subscribe_form = SubscribeForm(request.POST)
-
-        # Check if the form is valid
-        if subscribe_form.is_valid():
-            # Save the form data to the database
-            subscribe = subscribe_form.save()
-
-            # Display a success message to the user
-            messages.success(request, 'You have successfully subscribed!')
-
-            # Optionally, you can clear the form after successful submission
-            subscribe_form = SubscribeForm()
+        form = SubscribeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "You have successfully subscribed to our newsletter!")
+            return redirect('subscribe')  # Stay on the same page
     else:
-        # If the form has not been submitted, create an empty SubscribeForm
-        subscribe_form = SubscribeForm()
+        form = SubscribeForm()
 
-    # Set the template file for rendering
-    template = 'subscribe/subscribe.html'
-
-    # Prepare the context to be passed to the template
     context = {
-        'subscribe_form': subscribe_form,
+        'subscribe_form': form,
     }
-
-    # Render the template with the provided context
-    return render(request, template, context)
-            
-           
+    return render(request, 'subscribe/subscribe.html', context)
+  
