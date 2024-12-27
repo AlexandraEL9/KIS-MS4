0c64e33713c42315755b6542610b9d037c3e58e3
Alexandra Lee <alexandraelee9@gmail.com>
2024-12-27 15:57:46 +0000

    remove jest setup and package files

--- jest.setup.js
+++
@@ -1,6 +0,0 @@
-// Polyfill for TextEncoder in Node.js
-const { TextEncoder, TextDecoder } = require('util');
-const $ = require('jquery');
-global.$ = global.jQuery = $;
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
