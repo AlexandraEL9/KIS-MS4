4b7a6b1cb6ae74ac05cabcdc159fe871b3433650
Alexandra Lee <alexandraelee9@gmail.com>
2025-01-07 18:15:17 +0000

    update favourites views and toast success to handle faveourites toast with safe link

--- favourites/views.py
+++ favourites/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.urls import reverse
+from django.utils.html import format_html
 
 from .models import Favourite
 from products.models import Product
-
-# Create your views here.
 
 
 @login_required
@@ -33,22 +32,24 @@ def add_to_favourites(request, product_id):
         product=product
     )
 
+    favourites_url = reverse('favourites')  # Dynamically get the favourites URL
+
     if created:
         messages.success(
             request,
-            (
-                f"Added {product.name} to your favourites. "
-                "<a href='/favourites/' class='alert-link'>"
-                "View Favourites</a>"
+            format_html(
+                "Added {} to your favourites. <a href='{}' class='alert-link'>View Favourites</a>",
+                product.name,
+                favourites_url
             )
         )
     else:
         messages.info(
             request,
-            (
-                f"{product.name} is already in your favourites. "
-                "<a href='/favourites/' class='alert-link'>"
-                "View Favourites</a>"
+            format_html(
+                "{} is already in your favourites. <a href='{}' class='alert-link'>View Favourites</a>",
+                product.name,
+                favourites_url
             )
         )
 
@@ -67,12 +68,20 @@ def remove_from_favourites(request, product_id):
         favourite.delete()
         messages.success(
             request,
-            f"Removed {product.name} from your favourites."
+            format_html(
+                "Removed {} from your favourites. <a href='{}' class='alert-link'>View Favourites</a>",
+                product.name,
+                reverse('favourites')
+            )
         )
     except Favourite.DoesNotExist:
         messages.error(
             request,
-            f"{product.name} was not found in your favourites."
+            format_html(
+                "{} was not found in your favourites. <a href='{}' class='alert-link'>View Favourites</a>",
+                product.name,
+                reverse('favourites')
+            )
         )
 
     favourites = Favourite.objects.filter(user=request.user)
