a141d67db2b57d555ec7fb1d4712fb1480794d65
Alexandra Lee <alexandraelee9@gmail.com>
2025-01-17 13:07:15 +0000

    add admin for django admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ContactMessage
 
 # Register your models here.
+
+# contact model
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'submitted_at')
+    search_fields = ('name', 'email')
+    list_filter = ('submitted_at',)
