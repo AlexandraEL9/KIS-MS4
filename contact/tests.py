e2206e2fae7f2cbb84244064d0aa25c417e143a5
Alexandra Lee <alexandraelee9@gmail.com>
2025-01-17 13:33:42 +0000

    add 1st attempt at test of model

--- contact/tests.py
+++ contact/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import ContactMessage
 
 # Create your tests here.
+class ContactModelTests(TestCase):
+    def test_contact_message_creation(self):
+        message = ContactMessage.objects.create(
+            name="Test User",
+            email="test@example.com",
+            message="This is a test."
+        )
+        self.assertEqual(str(messge), "Message from Test User - test@example.com")
